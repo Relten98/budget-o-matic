e22728f45459fa1fd86d5b800110e545673a1bab
Relten98 <diezhandinoty@gmail.com>
2021-03-19 17:19:25 -0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,16 @@
-console.log('Service worker is good to go!');
+'use strict'
+
+console.log('Service worker is ready!');
+
+const FILES_TO_CACHE = [
+    `/db.js`,
+    `/index.html`,
+    `/index.js`,
+    `/index.css`,
+    `/manifest.webmanifest`,
+    `/icons/icon-192x192.png`,
+    `/icons/icon-512x512.png`
+];
+
+const STATIC_CACHE = `static-cache-v1`;
+const RUNTIME_CACHE = `runtime-cache`;
