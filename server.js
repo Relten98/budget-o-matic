94e12dca15f70a3f412867e19b0d1c193f29bdab
Relten98 <diezhandinoty@gmail.com>
2021-03-23 21:25:22 -0700

    Update server.js
    Deploying

--- server.js
+++ server.js
@@ -14,11 +14,15 @@ app.use(express.json());
 
 app.use(express.static(`public`));
 
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/budget`, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/budget',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 
 // routes
